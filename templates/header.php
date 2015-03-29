72ecd35c6fe965399770a839b5f2d97086969a48
otorcatf <oftc007@gmail.com>
2015-03-29 11:37:24 -0430

    Link for services added

--- templates/header.php
+++ templates/header.php
@@ -3,11 +3,16 @@
     <div class="ui stackable two column page grid">
         <div class="column">
             <a href="<?php bloginfo('url'); ?>">
-                <img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/info/images/bad-logo.png" />
+                <img class="logo" src="//placehold.it/250x100" />
             </a>
         </div>
         <div class="column">
             <div class="ui horizontal list">
+                <div class="item">
+                    <div class="content">
+                        <a href="#services">Services</a>
+                    </div>
+                </div>
                 <div class="item">
                     <div class="content">
                         <a href="#request">Request a travel</a>
