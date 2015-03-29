d83d275bd0f981135caf40b6ccd4727333c82078
otorcatf <oftc007@gmail.com>
2015-03-29 11:38:34 -0430

    Position changed to 25

--- lib/post-type/product.php
+++ lib/post-type/product.php
@@ -30,7 +30,7 @@ function setup_custom_post() {
         'show_in_menu' => true,
         'show_in_nav_menus' => true,
         'show_in_admin_bar' => true,
-        'menu_position' => 10,
+        'menu_position' => 25,
         'menu_icon' => 'dashicons-products',
         'can_export' => true,
         'has_archive' => true,
