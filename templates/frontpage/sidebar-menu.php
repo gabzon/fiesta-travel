ed977014018f23466aa87dad23fc011c3f4a96b6
otorcatf <oftc007@gmail.com>
2015-03-30 00:10:56 -0430

    Sidebar layout

---
+++ templates/frontpage/sidebar-menu.php
@@ -0,0 +1,18 @@
+<div class="ui left vertical inverted labeled icon sidebar menu">
+    <a href="#services" class="item">
+        <i class="star icon"></i>
+        Services
+    </a>
+    <a href="#request" class="item">
+        <i class="calendar icon"></i>
+        Request a travel
+    </a>
+    <a href="#about" class="item">
+        <i class="info icon"></i>
+        About
+    </a>
+    <a href="tel:022-305-3023" class="item">
+        <i class="call icon"></i>
+        022-305-3023
+    </a>
+</div>
