1075bbd59600a0b72f173b0fc04ffcbf2a08f397
otorcatf <oftc007@gmail.com>
2015-03-30 22:47:54 -0430

    Unecessary elements removed (Moved to base.php)

--- front-page.php
+++ front-page.php
@@ -1,7 +1,3 @@
-<?php get_template_part('templates/frontpage/sidebar-menu'); ?>
-<div class="pusher">
-<?php get_template_part('templates/header'); ?>
-
 <?php get_template_part('templates/frontpage/promotions'); ?>
 
 <?php get_template_part('templates/frontpage/services'); ?>
@@ -9,4 +5,3 @@
 <?php get_template_part('templates/frontpage/form'); ?>
 
 <?php get_template_part('templates/frontpage/about'); ?>
-</div>
