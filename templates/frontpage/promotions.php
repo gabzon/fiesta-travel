b9c56ebdf8c5a45a52fece6296cd759b641ee120
gabzon <gab.zambrano@gmail.com>
2015-04-03 15:05:28 +0200

    last changes

--- templates/frontpage/promotions.php
+++ templates/frontpage/promotions.php
@@ -1,5 +1,57 @@
 <?php $query = new WP_Query(array('post_type' => 'product')); ?>
 
+
+<div class="ui page grid">
+    <div class="column">
+        <div class="ui steps">
+          <div class="step">
+            <i class="truck icon"></i>
+            <div class="content">
+              <div class="title">Planning</div>
+
+            </div>
+          </div>
+          <div class="step">
+            <i class="payment icon"></i>
+            <div class="content">
+              <div class="title">Research</div>
+            </div>
+          </div>
+          <div class="step">
+            <i class="calendar icon"></i>
+            <div class="content">
+              <div class="title">Analyse</div>
+            </div>
+          </div>
+          <div class="active step">
+            <i class="calendar icon"></i>
+            <div class="content">
+              <div class="title">Creativity</div>
+            </div>
+          </div>
+          <div class="step">
+            <i class="calendar icon"></i>
+            <div class="content">
+              <div class="title">Design</div>
+            </div>
+          </div>
+          <div class="step">
+            <i class="mail icon"></i>
+            <div class="content">
+              <div class="title">Test</div>
+            </div>
+          </div>
+          <div class="step">
+            <i class="info icon"></i>
+            <div class="content">
+              <div class="title">Deploy</div>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
+
+
 <section id="promotion" class="ui basic inverted orange segment fullheight">
     <div class="segment-content">
         <h2 class="ui center aligned header uppercase"><?php _e('promotions'); ?></h2>
