78a2bf0780bd0f229fa4a69d1058719de54a6045
otorcatf <oftc007@gmail.com>
2015-04-20 14:21:26 -0430

    Picture profile uploader added

---
+++ piklist/parts/users/users-picture.php
@@ -0,0 +1,11 @@
+<?php
+
+piklist('field', array(
+    'type' => 'file',
+    'field' => 'user_picture',
+    'label' => __('Picture'),
+    'options' => array(
+        'modal_title' => __('Add Picture'),
+        'button' => __('Add Picture')
+    )
+));
