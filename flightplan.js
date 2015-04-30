208fd50e476b4ced463c9aa486880abb987eb7b9
gabzon <gab.zambrano@gmail.com>
2015-04-30 10:23:29 +0200

    photo jacqueline & flightplan

--- flightplan.js
+++ flightplan.js
@@ -3,11 +3,19 @@ var plan = require('flightplan');
 
 // configuration
 
-plan.target('default', [
+plan.target('production', [
   {
     host: 'vv.ftp.infomaniak.com',
     username: 'vv_listo_sevinci',
     agent: process.env.SSH_AUTH_SOCK,
-    webRoot:'/www/webapp/themes/fiesta-travel'
   }
 ]);
+
+// run commands on localhost
+plan.local(function(local) {
+
+  local.log('Copy files to remote hosts');
+  var filesToCopy = local.exec('git ls-files', {silent: true});
+  // rsync files to all the target's remote hosts
+  local.transfer(filesToCopy, '~/www/webapp/themes/fiesta-travel');
+});
